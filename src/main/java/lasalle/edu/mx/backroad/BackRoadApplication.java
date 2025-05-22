6816ccc837175675e0af15d6f31bdc6e4fc649e7
RendonSofia1 <rendonsofi03@gmail.com>
2025-05-22 17:01:45 -0600

    Primer commit

---
+++ src/main/java/lasalle/edu/mx/backroad/BackRoadApplication.java
@@ -0,0 +1,13 @@
+package lasalle.edu.mx.backroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRoadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lasalle/edu/mx/backroad/BackRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package lasalle.edu.mx.backroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
